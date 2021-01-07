9199b39352976ee12ebdb989f147e7c7d4d96919
doif-dobby <doif.dobby@gmail.com>
2021-01-07 19:45:01 +0900

    [#9]  - nginx 무중단 배포를 위한 스크립트 작성

---
+++ src/main/java/org/doif/projectv/business/IndexController.java
@@ -0,0 +1,16 @@
+package org.doif.projectv.business;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class IndexController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok("Project-V");
+    }
+}
