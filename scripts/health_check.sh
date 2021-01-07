3cbbbcc6b72e4bc69597507fc5cc75d4a1d9a25b
doif-dobby <doif.dobby@gmail.com>
2021-01-07 21:19:06 +0900

    [#9]  - nginx health check 시간 조정

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -14,7 +14,7 @@ fi
 
 echo "> Start health check of WAS at 'http://127.0.0.1:${TARGET_PORT}' ..."
 
-for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
+for RETRY_COUNT in `seq 1 100`;
 do
     echo "> #${RETRY_COUNT} trying..."
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:${TARGET_PORT}/health)
@@ -26,6 +26,6 @@ do
       echo "> Health check failed"
       exit 1
     fi
-    sleep 20
+    sleep 5
 done
         
